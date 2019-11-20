2026fc4e56d4d1ff60f6fc90b6cd50c57a9cab24
Iain Duff <iainrobertduff@gmail.com>
2019-11-20 18:31:27 +0400

    Adding responsive for footer

--- footer/footer.php
+++ footer/footer.php
@@ -11,7 +11,7 @@
 <li><a class="uk-text-bold" href="#"><span class="uk-margin-small-right uk-icon" uk-icon="receiver" style="color: #fff;"><svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="receiver"><path fill="none" stroke="#000" stroke-width="1.01" d="M6.189,13.611C8.134,15.525 11.097,18.239 13.867,18.257C16.47,18.275 18.2,16.241 18.2,16.241L14.509,12.551L11.539,13.639L6.189,8.29L7.313,5.355L3.76,1.8C3.76,1.8 1.732,3.537 1.7,6.092C1.667,8.809 4.347,11.738 6.189,13.611"></path></svg></span><p>Call Us <?php the_field('phone_number', 533); ?></p> <span style="font-weight: 400;"></span></a></li>
 </ul> -->
 </div>
-<div class="uk-flex-1 uk-margin-auto uk-visible@m">
+<div class="uk-flex-1 uk-margin-auto">
 
 <div uk-margin="" class="footer-nav">
 
@@ -27,7 +27,7 @@ wp_nav_menu( array(
 
 
 <!-- Social -->
-<div uk-grid class="uk-grid-medium uk-flex-middle uk-flex-center uk-grid uk-grid-stack uk-margin-top">
+<div uk-grid class="uk-grid-medium uk-flex-middle uk-flex-center uk-grid uk-grid-stack uk-margin-top uk-visible@m">
 
 <ul class="social-icons">
 
