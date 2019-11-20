f9ecb7fa042da580d11a080ea6370cd3f90445e3
Iain Duff <iainrobertduff@gmail.com>
2019-11-20 10:35:29 +0400

    adding twitter feeds to master

---
+++ twitter-feeds/twitter-feeds.php
@@ -0,0 +1,10 @@
+<!-- Stylesheet -->
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/components/twitter-feeds/twitter-feeds.css" type="text/css" media="screen" />
+<!--  -->
+
+<div class="twitter-feed uk-section">
+<div class="uk-container">
+<h1 class="uk-heading-divider uk-margin-medium-bottom">Latest Tweets</h1>
+<?php echo do_shortcode('[custom-twitter-feeds]'); ?>
+</div>
+</div>
